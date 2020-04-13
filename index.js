5cf05c08cf5e23ff852f1fef56f92c0012950a3d
Carlos Alberto Argüello Ramírez <deimagjas@gmail.com>
2020-04-13 15:27:40 -0500

    Agregando mensajes (#6)
    * se agregó un mensaje
    
    * se agregó un mensaje
    
    Co-authored-by: Carlos Alberto Argüello Ramírez <innovation@qubits.cloud>

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ app.get('/lucky', (req, res) => {
 
 app.get('/isi', (req, res) => {
   console.log('Hola desde Ingeniería de software I');
-  res.send('=========> ISI Rocks!');  
+  res.send('=========> ISI Rocks!');
+  res.send('=========> ISI Rocks twice!');  
 });
 
 app.get('/random', function(req, res) {
