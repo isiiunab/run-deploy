5ecc6416ae8e6581c5721ee5016ba0a15d395cf0
Cristhian González <chistrian101@hotmail.com>
2020-04-13 19:02:55 -0500

    Se modificó linea 31 de código de isi a isoftware #1

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get('/lucky', (req, res) => {
 
 });
 
-app.get('/isi', (req, res) => {
+app.get('/isoftware', (req, res) => {
   console.log('Hola desde Ingeniería de software I');
   res.send('=========> ISI Rocks!');
   res.send('=========> ISI Rocks twice!');
