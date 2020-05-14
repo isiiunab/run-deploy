d7f3a1bf116f505de6a83fba8b78afc72d9de8e2
carguello <carguello519@unab.edu.co>
2020-05-14 09:33:14 -0500

    agregando new endpoint

--- test/index.test.js
+++ test/index.test.js
@@ -2,9 +2,18 @@ const app = require('../index');
 const supertest = require('supertest');
 const request = supertest(app);
 
-it('Probando endpoint /is async', async done => {
-    const res = await request.get('/isi');
-    expect(res.statusCode).toBe(200);
-    expect(res.body.mensaje).toBe('===> Ingeniería de software!');
-    done();
-});
+
+describe('Probanod endpoints', () => {
+    it('Probando endpoint /isi async', async done => {
+        const res = await request.get('/isi');
+        expect(res.statusCode).toBe(200);
+        expect(res.body.mensaje).toBe('===> Ingeniería de software!');
+        done();
+    });
+    it('Probando endpoint /echo', async done => {
+        const res = await request.get('/echo');
+        expect(res.statusCode).toBe(200);        
+        expect(res.body.mensaje).toBe('echo');  
+        done();      
+    });
+})
